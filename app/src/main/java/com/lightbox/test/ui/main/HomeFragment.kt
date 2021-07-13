5c1de329f862df14797afb4a7893119a881c49bd
oscar <oscar.valverde@avantica.net>
2021-07-12 21:09:56 -0600

    My initial commit with the code

---
+++ app/src/main/java/com/lightbox/test/ui/main/HomeFragment.kt
@@ -0,0 +1,56 @@
+package com.lightbox.test.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import androidx.constraintlayout.helper.widget.Flow
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.lightbox.test.R
+import com.lightbox.test.databinding.ButtonLayoutBinding
+import com.lightbox.test.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        binding = HomeFragmentBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        setUpButtons()
+    }
+
+    private fun setUpButtons() {
+        addButton(0.3F, getString(R.string.title_1), getString(R.string.subtitle_1))
+        addButton(0.4F, getString(R.string.title_2), getString(R.string.subtitle_2))
+        addButton(0.3F, getString(R.string.title_3), getString(R.string.subtitle_3))
+        addButton(0.4F, getString(R.string.title_4), getString(R.string.subtitle_4))
+        addButton(0.3F, getString(R.string.title_5), getString(R.string.subtitle_5))
+    }
+
+    fun addButton(width: Float, title: String, subTitle: String) {
+        var buttonBinding: ButtonLayoutBinding = ButtonLayoutBinding.inflate(layoutInflater)
+        val view = buttonBinding.root
+        buttonBinding.textViewButtonTitle.text = title
+        buttonBinding.textViewButtonSubtitle.text = subTitle
+        view.layoutParams = ConstraintLayout.LayoutParams(0, WRAP_CONTENT)
+        view.id = View.generateViewId()
+        var set = ConstraintSet()
+        set.constrainPercentWidth(view.id, width)
+        binding.main.addView(view)
+        set.applyTo(binding.main)
+        binding.homeFlow.addView(view)
+    }
+
+}
---
+++ app/src/main/java/com/lightbox/test/ui/main/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.lightbox.test.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+
+}

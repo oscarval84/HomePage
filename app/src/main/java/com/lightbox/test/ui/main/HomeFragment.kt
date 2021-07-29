604e1a5a06fd1fbdb41bd726da057f53c6615207
oscar <oscar.valverde@avantica.net>
2021-07-29 16:25:26 -0600

    Adding changes for the UI improvement
    Including some icons to the project

--- app/src/main/java/com/lightbox/test/ui/main/HomeFragment.kt
+++ app/src/main/java/com/lightbox/test/ui/main/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import androidx.constraintlayout.helper.widget.Flow
@@ -28,22 +29,29 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        lifecycle.addObserver(viewModel)
         setUpButtons()
     }
 
     private fun setUpButtons() {
-        addButton(0.3F, getString(R.string.title_1), getString(R.string.subtitle_1))
+        addButton(0.4F, getString(R.string.title_1), getString(R.string.subtitle_1))
         addButton(0.4F, getString(R.string.title_2), getString(R.string.subtitle_2))
-        addButton(0.3F, getString(R.string.title_3), getString(R.string.subtitle_3))
+        addButton(0.4F, getString(R.string.title_3), getString(R.string.subtitle_3))
         addButton(0.4F, getString(R.string.title_4), getString(R.string.subtitle_4))
-        addButton(0.3F, getString(R.string.title_5), getString(R.string.subtitle_5))
+        addButton(0.4F, getString(R.string.title_5), getString(R.string.subtitle_5))
     }
 
-    fun addButton(width: Float, title: String, subTitle: String) {
+    private fun addButton(width: Float, title: String, subTitle: String) = addButton(width, title, subTitle, null)
+
+    private fun addButton(width: Float, title: String, subTitle: String, icon: Int?) {
         var buttonBinding: ButtonLayoutBinding = ButtonLayoutBinding.inflate(layoutInflater)
         val view = buttonBinding.root
         buttonBinding.textViewButtonTitle.text = title
         buttonBinding.textViewButtonSubtitle.text = subTitle
+        icon?.let {
+            buttonBinding.imageViewFront.visibility = VISIBLE
+            buttonBinding.imageViewFront.setImageResource(icon)
+        }
         view.layoutParams = ConstraintLayout.LayoutParams(0, WRAP_CONTENT)
         view.id = View.generateViewId()
         var set = ConstraintSet()
--- app/src/main/java/com/lightbox/test/ui/main/HomeViewModel.kt
+++ app/src/main/java/com/lightbox/test/ui/main/HomeViewModel.kt
@@ -1,8 +1,14 @@
 package com.lightbox.test.ui.main
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel : ViewModel(), LifecycleObserver {
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onCreate() {
+
+    }
 }
